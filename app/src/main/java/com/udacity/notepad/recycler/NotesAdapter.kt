f38cf9d2a18caa7fef5cdeff47fbc0b391816f78
Cassio Oliveira <cassio.o@bookatiger.com>
2017-12-14 15:38:03 +0100

    NotesAdapter. Cleaning up NotesViewHolder (by eradicating the findViewById call);

--- app/src/main/java/com/udacity/notepad/recycler/NotesAdapter.kt
+++ app/src/main/java/com/udacity/notepad/recycler/NotesAdapter.kt
@@ -7,10 +7,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.udacity.notepad.R
 import com.udacity.notepad.data.DataStore
 import com.udacity.notepad.data.Note
+import kotlinx.android.synthetic.main.item_note.view.*
 import java.util.*
 
 class NotesAdapter(private val context: Context) : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
@@ -57,11 +57,6 @@ class NotesAdapter(private val context: Context) : RecyclerView.Adapter<NotesAda
     }
 
     class NotesViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        internal var text: TextView
-
-        init {
-            text = itemView.findViewById(R.id.text)
-        }
+        val text = itemView.text
     }
 }
